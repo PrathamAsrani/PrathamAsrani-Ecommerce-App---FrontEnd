2cd7d361cbd3e9a1e19a09735d70e1d60b1d39c9
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-05-22 16:16:33 +0530

    Started Client Side working and Generated Layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Layout from "./Layout/Layout.jsx";
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <h1>hii</h1>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
