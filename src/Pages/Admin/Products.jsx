f9b3a8a836dba099ff62f78e7912a648758c22fc
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-07-25 18:20:50 +0530

    Applied Filters for the product the testing of the filters is successfully done all are workibg fine

--- src/Pages/Admin/Products.jsx
+++ src/Pages/Admin/Products.jsx
@@ -41,7 +41,7 @@ const Products = () => {
                                     <img src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`} className='card-img-top' alt={p.name} />
                                     <div className="card-body">
                                         <h5 className="card-title">{p.name}</h5>
-                                        <p className="card-text">{p.description}</p>
+                                        <p className="card-text">{p.description.substring(0, 60)}</p>
                                     </div>
                                 </div>
                             </Link>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -45,8 +45,23 @@ const HomePage = () => {
   }, [])
 
   useEffect(() => {
-    getAllProducts()
-  }, [])
+    if(checked.length == 0 && radio.length == 0) 
+        getAllProducts()
+  }, [checked.length, radio.length])
+
+  useEffect(() => {
+    if(checked.length || radio.length) 
+      filterProduct()
+  }, [checked, radio])
+
+  const filterProduct = async() => {
+    try{
+      const {data} = await axios.post(`${process.env.REACT_APP_API}/api/v1/product/product-filters`, {checked, radio})
+      setProducts(data?.products)
+    }catch(err){
+      console.log(err);
+    }
+  }
 
   return (
     <Layout title={"All Products - Best offers"} >
@@ -72,6 +87,9 @@ const HomePage = () => {
               ))}
             </Radio.Group>
           </div>
+          <div className="d-flex flex-column">
+            <button className='btn btn-danger' onClick={() => window.location.reload()} >Reset Filter</button>
+          </div>
         </div>
         <div className="col-md-9">
           <h1 className='text-center'>All Products</h1>
@@ -81,7 +99,8 @@ const HomePage = () => {
                 <img src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`} className='card-img-top' alt={p.name} />
                 <div className="card-body">
                   <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">{p.description}</p>
+                  <p className="card-text">{p.description.substring(0, 30)}</p>
+                  <p className="card-text">${p.price}</p>
                   <button href="#" className='btn ms-1 btn-primary'>More Details</button>
                   <button href="#" className='btn ms-1 btn-secondary'>Add to Cart</button>
                 </div>
